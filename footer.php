69a304d0a809a31c503ee941e6742c8a9bab1d23
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-04-04 23:20:13 +0530

    enqueued footer style

--- footer.php
+++ footer.php
@@ -1,3 +1,49 @@
 <?php
 wp_footer();
 ?>
+            <footer>
+                <div class="left">
+                    <div class="head">
+                        <figure>
+                            <img src="<?php echo get_template_directory_uri().'/assets/images/logo.png' ?>">
+                        </figure>
+                        <h2><nobr style="font-weight:lighter;">Lasles<strong>VPN</strong></nobr></h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur. Quis tortor gravida nibh arcu id purus ullamcorper. Vel vel erat semper augue.
+                    </p>
+                    <div class="social">
+                        <img src="<?php echo get_template_directory_uri().'/assets/images/fblogo.png' ?>">
+                        <img src="<?php echo get_template_directory_uri().'/assets/images/walogo.png' ?>">
+                        <img src="<?php echo get_template_directory_uri().'/assets/images/utlogo.png' ?>">
+                    </div>
+                </div>
+                <div class="right">
+                    <div>
+                        <p><strong>Products</strong></p>
+                        <p>Download</p>
+                        <p>Pricing</p>
+                        <p>Locations</p>
+                        <p>Server</p>
+                        <p>Countries</p>
+                        <p>Blog</p>
+                    </div>
+                    <div>
+                        <p><strong>Engage</strong></p>
+                        <p>LaslesVPN</p>
+                        <p>FAQ</p>
+                        <p>Tutorials</p>
+                        <p><nobr>About Us</nobr></p>
+                        <p><nobr>Privacy Policy</nobr></p>
+                        <p><nobr>Terms of service</nobr></p>
+                    </div>
+                    <div>
+                        <p><strong><nobr>Terms of service</nobr></strong></p>
+                        <p><nobr>Become Partner</nobr></p>
+                        <p>Affilite</p>
+                    </div>
+                </div>
+            </footer>
+        </main>
+    </body>
+</html>
--- functions.php
+++ functions.php
@@ -6,6 +6,8 @@ add_theme_support( 'title-tag' );
 function laslesvpn_enqueue_styles() {
 
     wp_enqueue_style( 'nav-css', get_template_directory_uri() . '/assets/css/nav.css' );
+    wp_enqueue_style( 'footer-css', get_template_directory_uri() . '/assets/css/footer.css' );
+    wp_enqueue_style( 'font-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'laslesvpn_enqueue_styles' );
