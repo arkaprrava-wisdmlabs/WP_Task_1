26e061ef14a6a1766aa74ee361c81aaa3eb78be8
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-04-04 23:13:58 +0530

    created navbar

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@
 add_theme_support( 'custom-logo');
 add_theme_support( 'title-tag' );
 
+function laslesvpn_enqueue_styles() {
+
+    wp_enqueue_style( 'nav-css', get_template_directory_uri() . '/assets/css/nav.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'laslesvpn_enqueue_styles' );
 ?>
--- header.php
+++ header.php
@@ -1,3 +1,45 @@
 <?php
 wp_head();
 ?>
+<html>
+    <head>
+        <title> <?php bloginfo( 'name' ); ?> </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body <?php body_class();?> >
+        <nav>
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/logo.png' ?>">
+            </figure>
+            <div class="logo-text">
+                <p>Lasles<span style="font-weight:bold">VPN</span></p>
+            </div>
+            <div class="links">
+                <div>
+                    features
+                </div>
+                <div>
+                    Pricing
+                </div>
+                <div>
+                    testimonial
+                </div>
+                <div>
+                    help
+                </div>
+            </div>
+            <div class="sign">
+                <div>
+                    <nobr>Sign in</nobr>
+                </div>
+                <div class="button">
+                    <nobr>Sign Up</nobr>
+                </div>
+            </div>
+            <div class="drop">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+        <main>
