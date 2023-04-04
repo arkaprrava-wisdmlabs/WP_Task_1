4f01c77cdd11b3d8f3d7c4f03ac6104d64ba5871
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-04-04 23:29:02 +0530

    added responsive info section

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     get_header();
 ?>
     <?php get_template_part('template-parts/content','header') ?>
+    <?php get_template_part('template-parts/content','info') ?>
 <?php
     get_footer();
 ?>
---
+++ template-parts/content-info.php
@@ -0,0 +1,31 @@
+    <section class="card" style="position:relative;top:-3%;" id="info">
+        <div class="content">
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/user.png' ?>" alt="user-icon" srcset="">
+            </figure>
+            <div>
+                <h2>Users</h2>
+                <p>30,000+</p>
+            </div>
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/Line 1.png' ?>" alt="line" srcset="">
+            </figure>
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/server.png' ?>" alt="server-icon" srcset="">
+            </figure>
+            <div>
+                <h2>Companies</h2>
+                <p>30+</p>
+            </div>
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/Line 1.png' ?>" alt="line" srcset="">
+            </figure>
+            <figure>
+                <img src="<?php echo get_template_directory_uri().'/assets/images/loc.png' ?>" alt="location-icon" srcset="">
+            </figure>
+            <div>
+                <h2>Countries</h2>
+                <p>30+</p>
+            </div>
+        </div>
+    </section>
